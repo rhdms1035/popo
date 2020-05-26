a3cd9b46d670c5e97995dfb4bdd08591824e56ba
rhdms1035 <kkokkoning@naver.com>
2020-05-26 18:24:02 +0900

    5월26일햄버거메뉴중

--- js/myscript.js
+++ js/myscript.js
@@ -12,3 +12,12 @@ $(".tabmenu > li").on("click", function(){
     $(this).addClass("active")
     $(this).siblings().removeClass("active")
 })
+
+// 작은화면에서 햄버거모양 누르면 메뉴가나옴
+$("#header .openMOgnb").on("click", function(){
+   $("#gnb").show()
+})
+
+$("#header .closeMOgnb").on("click", function(){
+    $(this).parents("#header").removeClass("on")
+})
