95d01f0da4d5b62997876aeff2bd30d52e31b933
rhdms1035 <kkokkoning@naver.com>
2020-05-27 18:41:58 +0900

    5월27일반응형메뉴완성

--- js/myscript.js
+++ js/myscript.js
@@ -15,9 +15,25 @@ $(".tabmenu > li").on("click", function(){
 
 // 작은화면에서 햄버거모양 누르면 메뉴가나옴
 $("#header .openMOgnb").on("click", function(){
+    $("#header").addClass("on")
    $("#gnb").show()
 })
 
+// 햄버거모양을 누르면 메뉴와 엑스버튼이 나오는데
+// 엑스버튼을 누르면 메뉴창이 닫힘
 $("#header .closeMOgnb").on("click", function(){
-    $(this).parents("#header").removeClass("on")
+    $("#header").removeClass("on")
+    $("#gnb").hide()
+})
+
+// 작은 메뉴창이 닫히고 창이 커지면 메뉴바가 안나오는걸 다시 불러옴
+// 799사이즈 이상이 되면 메뉴를 다시 보여줌
+$(window).resize(function(){
+    var ww = $(this).width()
+    if (ww > 799) {
+        $("#header").removeClass("on")
+        $("#gnb").show()
+    } else {
+        $("#gnb").hide()
+    }
 })
